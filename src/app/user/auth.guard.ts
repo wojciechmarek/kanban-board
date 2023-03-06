b6cfd89a5716a07b3366decf88ee5db11922a9f2
Wojciech Marek <marek.wojciech@outlook.com>
2023-03-06 23:39:22 +0100

    feat: add auth guard

---
+++ src/app/user/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { FirebaseApp } from '@angular/fire/app';
+import { Auth, getAuth } from '@angular/fire/auth';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { SnackService } from '../services/snack.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  auth: Auth;
+
+  constructor(private afService: FirebaseApp, private snackBar: SnackService) {
+    this.auth = getAuth(this.afService);
+  }
+
+  async canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    console.log(this.auth.currentUser);
+
+    const isLoggedIn = !!this.auth.currentUser;
+
+    if (!isLoggedIn) {
+      this.snackBar.authError();
+    }
+
+    return isLoggedIn;
+  }
+}
