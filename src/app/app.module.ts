60cfeacbbf69ca9c132cba5061d9333631391a27
Wojciech Marek <marek.wojciech@outlook.com>
2023-03-06 12:45:07 +0100

    chore: configure a firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+
+import { environment } from 'src/environments/environment';
+
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
   imports: [
@@ -14,6 +20,9 @@ import { HomePageComponent } from './home-page/home-page.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDLVyJZWS6vvHfUErRAr6OW81o8e0hX87c',
+    authDomain: 'kanban-board-4e2ba.firebaseapp.com',
+    projectId: 'kanban-board-4e2ba',
+    storageBucket: 'kanban-board-4e2ba.appspot.com',
+    messagingSenderId: '712388493214',
+    appId: '1:712388493214:web:f7f38f1bc116bf5fd2a08f',
+    measurementId: 'G-2SQKZCV3M9',
+  },
+};
