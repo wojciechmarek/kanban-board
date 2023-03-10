ef9dd2bd8de007171b26cb8ba0284d6fd754fe4a
Wojciech Marek <marek.wojciech@outlook.com>
2023-03-10 11:04:28 +0100

    feat: add custom value accessor logic to custom stepper

--- src/app/custom-form/stepper/stepper.component.ts
+++ src/app/custom-form/stepper/stepper.component.ts
@@ -1,21 +1,63 @@
 import { Component, Input } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-stepper',
   templateUrl: './stepper.component.html',
   styleUrls: ['./stepper.component.scss'],
 })
-export class StepperComponent {
+export class StepperComponent implements ControlValueAccessor {
   quantity = 1;
 
   @Input()
   increment: number = 1;
 
+  onChange = (quantity: number) => {};
+
+  onTouched = () => {};
+
+  isTouched = false;
+
+  isDisabled = false;
+
   onSubtract() {
-    this.quantity -= this.increment;
+    this.markAsTouched();
+
+    if (!this.isDisabled) {
+      this.quantity -= this.increment;
+      this.onChange(this.quantity);
+    }
   }
 
   onAdd() {
-    this.quantity += this.increment;
+    this.markAsTouched();
+
+    if (!this.isDisabled) {
+      this.quantity += this.increment;
+      this.onChange(this.quantity);
+    }
+  }
+
+  markAsTouched() {
+    if (!this.isTouched) {
+      this.isTouched = true;
+      this.onTouched();
+    }
+  }
+
+  writeValue(quantity: number): void {
+    this.quantity = quantity;
+  }
+
+  registerOnChange(onChange: any): void {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any): void {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
   }
 }
